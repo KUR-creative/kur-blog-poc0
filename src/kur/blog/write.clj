05113e3fe9520d8cc1c7846de06d2c27bb7a8bce
kur <rhdnfka94@gmail.com>
2022-07-16 08:35:28 +0900

    Use hiccup instead of selmer

--- src/kur/blog/write.clj
+++ src/kur/blog/write.clj
@@ -1,10 +1,11 @@
 (ns kur.blog.write
   "Writes posts of blog from input md and resource"
   (:require [clojure.java.io :as io]
-            [selmer.parser :as parser]
-            [selmer.util :refer [without-escaping]])
+            [hiccup.core :refer [html]]
+            [hiccup.page :refer [doctype]])
   (:import (com.eclipsesource.v8 NodeJS)))
 
+;;
 (defonce nodejs-runtime (NodeJS/createNodeJS))
 (def md2x-path "./md2x/out/md2x.js")
 (def md2x (.require nodejs-runtime (io/file md2x-path)))
@@ -12,15 +13,21 @@
 (defn obsidian-html [md]
   (.executeJSFunction md2x "obsidian" (to-array [md])))
 
-(defn post [template {:keys [body prev-post next-post] :as content}]
-  (without-escaping (parser/render template content)))
+;;
+(def scale1-viewport
+  [:meta {:name "viewport"
+          :content "width=device-width, initial-scale=1"}])
+(defn page [head-xs body-xs] ;; TODO? [[xs] [ys]]
+  (list (doctype :html5) [:head head-xs] [:body body-xs]))
+
+;;
+(defn post [{:keys [content prev-link next-link archive-link]}]
+  (let [template (html (page (list scale1-viewport) (list "%s")))]
+    (format template content)))
 
 (comment
   (spit "out/t.html"
-        (post (slurp "resource/template/post.html")
-              {:body (obsidian-html (slurp "./README.md"))
-               :prev-post 1
-               :next-post 5}))
+        (post {:content (obsidian-html (slurp "./README.md"))}))
 
   #_((obsidian-html "### 3")
      (obsidian-html (slurp "./README.md"))
