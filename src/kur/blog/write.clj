3ead28c655f27fcf8d7f48452db8096e8f19ed20
kur <rhdnfka94@gmail.com>
2022-07-15 16:43:44 +0900

    Use selmer templating lib for post creation

--- src/kur/blog/write.clj
+++ src/kur/blog/write.clj
@@ -1,16 +1,28 @@
 (ns kur.blog.write
   "Writes posts of blog from input md and resource"
-  (:require [clojure.java.io :as io])
+  (:require [clojure.java.io :as io]
+            [selmer.parser :as parser]
+            [selmer.util :refer [without-escaping]])
   (:import (com.eclipsesource.v8 NodeJS)))
 
 (defonce nodejs-runtime (NodeJS/createNodeJS))
 (def md2x-path "./md2x/out/md2x.js")
 (def md2x (.require nodejs-runtime (io/file md2x-path)))
+
 (defn obsidian-html [md]
   (.executeJSFunction md2x "obsidian" (to-array [md])))
 
+(defn post [template {:keys [body prev-post next-post] :as content}]
+  (without-escaping (parser/render template content)))
+
 (comment
-  (obsidian-html "### 3")
-  (obsidian-html (slurp "./README.md"))
-  (spit "out/t.html" (obsidian-html (slurp "./README.md")))
+  (spit "out/t.html"
+        (post (slurp "resource/template/post.html")
+              {:body (obsidian-html (slurp "./README.md"))
+               :prev-post 1
+               :next-post 5}))
+
+  #_((obsidian-html "### 3")
+     (obsidian-html (slurp "./README.md"))
+     (spit "out/t.html" (obsidian-html (slurp "./README.md"))))
   )
