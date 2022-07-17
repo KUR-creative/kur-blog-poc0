2bb0be8fd284b2d782bed2d568443c31241d005d
kur <rhdnfka94@gmail.com>
2022-07-17 16:45:40 +0900

    Add meta, title spec

--- src/kur/blog/write.clj
+++ src/kur/blog/write.clj
@@ -5,9 +5,11 @@
             [clojure.spec.alpha :as s]
             [clojure.spec.gen.alpha :as sg]
             [clojure.string :as str]
+            [com.gfredericks.test.chuck.generators :as ug]
             [hiccup.core :refer [html]]
             [hiccup.element :refer [unordered-list link-to]]
-            [kur.util.time :refer [time-format]])
+            [kur.util.time :refer [time-format]]
+            [kur.util.regex :refer [hangul* alphanumeric*]])
   (:import (com.eclipsesource.v8 NodeJS)))
 
 ;; md2x
@@ -42,6 +44,20 @@
     #(sg/fmap (fn [[author ctime]] (str author ctime))
               (sg/tuple (s/gen :post/author) (s/gen :post/ctime)))))
 
+(s/def :post/meta-str ; + means public, else private.
+  #{"+" "-" ""})
+
+(def obsidian-title-symbol* #"[\!\,\ \.\+\=\-\_\(\)]*")
+(def gen-post-title
+  "<id>[.<meta>].<title>.md  NOTE: title can be empty string"
+  (sg/fmap (fn [[syms alphanums hanguls]]
+            (->> (str syms alphanums hanguls) vec shuffle (apply str)))
+          (sg/tuple (ug/string-from-regex obsidian-title-symbol*)
+                    (ug/string-from-regex alphanumeric*)
+                    (ug/string-from-regex hangul*))))
+(s/def :post/title
+  (s/with-gen string? (fn [] gen-post-title)))
+
 ;; Entities
 (defn post-info [md-path]
   (let [fname (fs/file-name md-path)
@@ -52,7 +68,7 @@
 (def scale1-viewport
   [:meta {:name "viewport"
           :content "width=device-width, initial-scale=1"}])
-
+          
 ;; Make html
 (defn optional-link
   ([url] (optional-link url url url))
@@ -73,10 +89,6 @@
                      (unordered-list (map optional-link post-links)))]))
 
 (comment
-  (s/exercise :post/ctime 20)
-  (s/exercise :post/author 20)
-  (s/exercise :post/id 20)
-
   (def md-paths (fs/list-dir "./test/fixture/blog-v1-md"))
   (id-info "asd1234567890")
   (map post-info md-paths)
@@ -93,4 +105,17 @@
   #_((obsidian-html "### 3")
      (obsidian-html (slurp "./README.md"))
      (spit "out/t.html" (obsidian-html (slurp "./README.md"))))
-  )
+
+  (str/join " " (->> (fs/list-dir "/home/dev/outer-brain/thinks/")
+                     (map fs/file-name) (map set)
+                     (apply clojure.set/union) (sort)))
+
+  (s/exercise :post/ctime 20)
+  (s/exercise :post/author 20)
+  (s/exercise :post/id 20)
+  (s/exercise :post/meta-str)
+
+  (sg/sample (ug/string-from-regex obsidian-title-symbol*) 30)
+  (sg/sample gen-post-title 30)
+  (s/exercise :post/title)
+  )  
