5dd939b9136d48a43e3436df5660fba5aae2461b
kur <rhdnfka94@gmail.com>
2022-07-18 14:29:43 +0900

    Add post file name <-> parts round trip and its test

--- src/kur/blog/post.clj
+++ src/kur/blog/post.clj
@@ -1,11 +1,13 @@
 (ns kur.blog.post
   "Blog post entity"
-  (:require [clojure.spec.alpha :as s]
+  (:require [babashka.fs :as fs]
+            [clojure.spec.alpha :as s]
             [clojure.spec.gen.alpha :as sg]
             [clojure.string :as str]
             [com.gfredericks.test.chuck.generators :as ug]
             [kur.util.time :refer [time-format]]
-            [kur.util.regex :refer [hangul* alphanumeric*]]))
+            [kur.util.regex :refer [hangul* alphanumeric*]]
+            [medley.core :refer [assoc-some]]))
 
 (defn digit? [c] ; TODO: move to util?
   (and (>= 0 (compare \0 c)) (>= 0 (compare c \9))))
@@ -37,19 +39,44 @@
 (def gen-post-title
   "<id>[.<meta>].<title>.md  NOTE: title can be empty string"
   (sg/fmap (fn [[syms alphanums hanguls]]
-            (->> (str syms alphanums hanguls) vec shuffle (apply str)))
-          (sg/tuple (ug/string-from-regex obsidian-title-symbol*)
-                    (ug/string-from-regex alphanumeric*)
-                    (ug/string-from-regex hangul*))))
+             (->> (str syms alphanums hanguls) vec shuffle (apply str)))
+           (sg/tuple (ug/string-from-regex obsidian-title-symbol*)
+                     (ug/string-from-regex alphanumeric*)
+                     (ug/string-from-regex hangul*))))
 (s/def ::title
-  (s/with-gen (s/and string? 
-                     #(not (s/valid? ::meta-str 
+  (s/with-gen (s/and string?
+                     #(not (s/valid? ::meta-str
                                      (first (str/split % #"\." 2)))))
     (fn [] gen-post-title)))
 
-(s/def :post.fname/id+title
+(s/def ::file-name-parts
+  (s/keys :req [::id] :opt [::meta-str ::title]))
+
+(def post-extension "md")
+
+(defn parts->fname [fname-parts]
+  (str (->> fname-parts
+            ((juxt ::id ::meta-str ::title))
+            (remove nil?)
+            (str/join ".")) "." post-extension))
+            
+(defn fname->parts [post-fname]
+  (let [base-name (fs/strip-ext post-fname)
+        [id meta title] (str/split base-name #"\." 3)
+        ret {::id id}]
+    (if (s/valid? ::meta-str meta)
+      (assoc-some ret ::meta-str meta ::title title)
+      (if (s/valid? ::title meta)
+        (assoc ret ::title (if title (str meta "." title) meta))
+        ret))))
+
+(s/def ::file-name
   (s/with-gen string?
-    #(sg/fmap (fn [[id title]] (str id "." title)))))
+    #(sg/fmap parts->fname (s/gen ::file-name-parts))))
+
+(defn file-name-info [post-fname]
+  (let [parts (str/split post-fname #"\.")]
+    parts))
 
 (comment
   (id-info "asd1234567890")
@@ -65,9 +92,19 @@
   (s/explain ::title "-.asdf")
   (s/explain ::title ".asdf")
 
+  (s/exercise ::file-name-parts 20)
+  (sg/sample (s/gen ::file-name-parts) 20)
+  (sg/sample (s/gen ::file-name) 20)
+  (map #(vector % (file-name-info %))
+       (sg/sample (s/gen ::file-name) 20))
+  (s/explain ::file-name "kur1234567890.md")
+
+  (require '[clojure.test :refer [is]])
+  (doseq [parts (reverse (sg/sample (s/gen ::file-name-parts) 1000))]
+    (is (= parts (fname->parts (parts->fname parts)))
+        (str "\"" (parts->fname parts) "\"")))
 
   #_(str/join " " ;; To know used characters
               (->> (fs/list-dir "/home/dev/outer-brain/thinks/")
                    (map fs/file-name) (map set)
-                   (apply clojure.set/union) (sort)))
-  )
+                   (apply clojure.set/union) (sort))))
