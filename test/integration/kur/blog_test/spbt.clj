d3a5f27c8c750256ab7e81128152d2513327ff73
kur <rhdnfka94@gmail.com>
2022-07-30 10:55:33 +0900

    Add generators for C/R/D ops

--- test/integration/kur/blog_test/spbt.clj
+++ test/integration/kur/blog_test/spbt.clj
@@ -2,13 +2,14 @@
   "Blog Integration Test Using Stateful PBT"
   (:require
    [clojure.spec.alpha :as s]
+   [clojure.string :as str]
    [clojure.test.check.generators :as g]
    [kur.blog.post :as post]
+   [kur.blog.reader :refer [url-path-set]]
    [kur.util.generator :refer [string-from-regexes]]
-   [kur.util.regex :refer [ascii* common-whitespace* hangul*]]
-   ))
+   [kur.util.regex :refer [ascii* common-whitespace* hangul*]]))
 
-;; Generators and Specs
+;;; Generators and Specs
 (s/def ::md-text
   (s/with-gen string?
     #(string-from-regexes ascii* common-whitespace* hangul*)))
@@ -16,10 +17,31 @@
 (s/def ::post (s/keys :req [::post/public? ::post/title ::md-text]))
 (s/def ::state (s/map-of ::post/id ::post))
 
-;;
+;; Operations
+(defn gen-create [state]
+  (g/fmap #(vector :create (key %) (val %)) (g/elements state)))
+
+(defn gen-delete [state]
+  (g/fmap #(vector :delete (key %)) (g/elements state)))
+
+(defn url [scheme ip port path]
+  (str scheme "://" ip ":" port "/" path))
+(defn gen-valid-url [state]
+  (g/fmap #(url "http" "localhost" 8080 %)
+          (g/elements (url-path-set state))))
+(defn gen-invalid-url [state]
+  (->> (s/gen (s/or :i+t (s/cat :i ::post/id :t (s/? ::post/title))
+                    :s (s/tuple (s/and string? seq))))
+       (g/fmap #(str/join "." %))
+       (g/such-that #(not (contains? (url-path-set state) %)))
+       (g/fmap #(url "http" "localhost" 8080 %))))
+(defn gen-read [state]
+  (g/fmap #(vector :read %)
+          (g/one-of [(gen-valid-url state) (gen-invalid-url state)])))
+
+;;;
 (comment
-  #_(require '[babashka.fs :as fs]
-           '[clojure.string :as str])
+  #_(require '[babashka.fs :as fs])
   #_(str/join " " ;; To know used characters
               (->> (fs/list-dir "/home/dev/outer-brain/thinks/")
                    (map #(-> % str slurp set))
@@ -27,4 +49,15 @@
   (g/sample (s/gen ::md-text) 30)
   (g/sample (s/gen ::post) 30)
   (g/sample (s/gen ::state) 10)
-  )
+
+  ;; Operations
+  (def state (last (g/sample (s/gen ::state))))
+  (url-path-set state) ;; NOTE: unit test for reader/url-path-set
+
+  (g/sample (gen-create state) 2)
+  (g/sample (gen-delete state) 2)
+
+  (url "http" "localhost" 8384 "")
+  (g/sample (gen-valid-url state) 20)
+  (g/sample (gen-invalid-url state) 20)
+  (g/sample (gen-read state) 20))
