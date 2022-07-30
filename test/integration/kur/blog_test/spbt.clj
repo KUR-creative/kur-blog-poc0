447fd40c1475777f1e0dc4a9e616977c65e27f12
kur <rhdnfka94@gmail.com>
2022-07-30 13:16:20 +0900

    Relocate gen code to proper section, Add test code

--- test/integration/kur/blog_test/spbt.clj
+++ test/integration/kur/blog_test/spbt.clj
@@ -17,15 +17,9 @@
 (s/def ::post (s/keys :req [::post/public? ::post/title ::md-text]))
 (s/def ::state (s/map-of ::post/id ::post))
 
-;; Operations
-(defn gen-create [state]
-  (g/fmap #(vector :create (key %) (val %)) (g/elements state)))
-
-(defn gen-delete [state]
-  (g/fmap #(vector :delete (key %)) (g/elements state)))
-
 (defn url [scheme ip port path]
   (str scheme "://" ip ":" port "/" path))
+
 (defn gen-valid-url [state]
   (g/fmap #(url "http" "localhost" 8080 %)
           (g/elements (url-path-set state))))
@@ -35,6 +29,14 @@
        (g/fmap #(str/join "." %))
        (g/such-that #(not (contains? (url-path-set state) %)))
        (g/fmap #(url "http" "localhost" 8080 %))))
+
+;;; Operations
+(defn gen-create [state]
+  (g/fmap #(vector :create (key %) (val %)) (g/elements state)))
+
+(defn gen-delete [state]
+  (g/fmap #(vector :delete (key %)) (g/elements state)))
+
 (defn gen-read [state]
   (g/fmap #(vector :read %)
           (g/one-of [(gen-valid-url state) (gen-invalid-url state)])))
@@ -42,6 +44,7 @@
 (defn gen-ops [state]
   (g/vector (g/one-of [(gen-create state) (gen-read state)
                        (gen-delete state)])))
+
 ;;;
 (comment
   #_(require '[babashka.fs :as fs])
@@ -49,19 +52,23 @@
               (->> (fs/list-dir "/home/dev/outer-brain/thinks/")
                    (map #(-> % str slurp set))
                    (apply clojure.set/union) (sort)))
+
+  (def state (last (g/sample (s/gen ::state))))
+
+  ;;; gens & specs
   (g/sample (s/gen ::md-text) 30)
   (g/sample (s/gen ::post) 30)
   (g/sample (s/gen ::state) 10)
 
-  ;; Operations
-  (def state (last (g/sample (s/gen ::state))))
-  (url-path-set state) ;; NOTE: unit test for reader/url-path-set
-
-  (g/sample (gen-create state) 2)
-  (g/sample (gen-delete state) 2)
-
   (url "http" "localhost" 8384 "")
   (g/sample (gen-valid-url state) 20)
   (g/sample (gen-invalid-url state) 20)
+
+  ;;; Operations
+  (url-path-set state) ;; NOTE: unit test for reader/url-path-set
+
+  (g/sample (gen-create state) 10)
+  (g/sample (gen-delete state) 10)
+
   (g/sample (gen-read state) 20)
   (g/sample (gen-ops state) 20))
