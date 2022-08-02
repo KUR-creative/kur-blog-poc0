e8abe05c1d1cd0d8ff00255ba801a0fb196de580
kur <rhdnfka94@gmail.com>
2022-08-02 10:45:07 +0900

    Correct name: state -> id:post
    Previously, state was not the state. It was just post candidates pool.

--- test/integration/kur/blog_test/spbt.clj
+++ test/integration/kur/blog_test/spbt.clj
@@ -16,41 +16,46 @@
     #(string-from-regexes ascii* common-whitespace* hangul*)))
 
 (s/def ::post (s/keys :req [::post/public? ::post/title ::md-text]))
-(s/def ::state (s/map-of ::post/id ::post))
+(s/def ::id:post (s/map-of ::post/id ::post :min-count 1))
 
 (defn url [scheme ip port path]
   (str scheme "://" ip ":" port "/" path))
 
-(defn gen-valid-url [state]
+(defn gen-valid-url [id:post]
   (g/fmap #(url "http" "localhost" 8080 %)
-          (g/elements (url-path-set state))))
-(defn gen-invalid-url [state]
+          (g/elements (url-path-set id:post))))
+(defn gen-invalid-url [id:post]
   (->> (s/gen (s/or :i+t (s/cat :i ::post/id :t (s/? ::post/title))
                     :s (s/tuple (s/and string? seq))))
        (g/fmap #(str/join "." %))
-       (g/such-that #(not (contains? (url-path-set state) %)))
+       (g/such-that #(not (contains? (url-path-set id:post) %)))
        (g/fmap #(url "http" "localhost" 8080 %))))
 
 ;;; Operations
-(defn gen-create [state]
+(defn gen-create [id:post]
   (g/fmap #(hash-map :op :create, :id (key %), :post (val %))
-          (g/elements state)))
+          (g/elements id:post)))
 
-(defn gen-read [state]
+(defn gen-read [id:post]
   (g/let [[url valid?]
-          (g/one-of [(g/tuple (gen-valid-url state) (g/return true))
-                     (g/tuple (gen-invalid-url state) (g/return false))])]
+          (g/one-of [(g/tuple (gen-valid-url id:post) (g/return true))
+                     (g/tuple (gen-invalid-url id:post) (g/return false))])]
     (if valid?
       (let [id (::post/id (-> url fs/file-name post/fname->parts))]
-        {:op :read, :url url, :id id, :post (state id)})
+        {:op :read, :url url, :id id, :post (id:post id)})
       {:op :read, :url url})))
 
-(defn gen-delete [state]
-  (g/fmap #(hash-map :op :delete :id (key %)) (g/elements state)))
+(defn gen-delete [id:post]
+  (g/fmap #(hash-map :op :delete :id (key %)) (g/elements id:post)))
+  
+#_(def gen-num-publics
+  (g/return {:op :num-publics}))
 
-(defn gen-ops [state]
-  (g/vector (g/one-of [(gen-create state) (gen-read state)
-                       (gen-delete state)])))
+(defn gen-ops [id:post]
+  (g/vector (g/one-of [(gen-create id:post) 
+                       #_(gen-read id:post)
+                       (gen-delete id:post)
+                       ])))
 
 ;;; Runners
 (defn run-model [state ops]
@@ -70,25 +75,27 @@
                    (map #(-> % str slurp set))
                    (apply clojure.set/union) (sort)))
 
-  (def state (last (g/sample (s/gen ::state))))
+  (def id:post (last (g/sample (s/gen ::id:post))))
 
   ;;; gens & specs
   (g/sample (s/gen ::md-text) 30)
   (g/sample (s/gen ::post) 30)
-  (g/sample (s/gen ::state) 10)
+  (g/sample (s/gen ::id:post) 10)
 
   (url "http" "localhost" 8384 "")
-  (g/sample (gen-valid-url state) 20)
-  (g/sample (gen-invalid-url state) 20)
+  (g/sample (gen-valid-url id:post) 20)
+  (g/sample (gen-invalid-url id:post) 20)
 
   ;;; Operations
-  (url-path-set state) ;; NOTE: unit test for reader/url-path-set
+  (url-path-set id:post) ;; NOTE: unit test for reader/url-path-set
 
-  (g/sample (gen-create state) 10)
-  (g/sample (gen-read state) 20)
-  (g/sample (gen-delete state) 10)
+  (g/sample (gen-create id:post) 10)
+  (g/sample (gen-read id:post) 20)
+  (g/sample (gen-delete id:post) 10)
+  #_(g/sample gen-num-publics)
 
-  (g/sample (gen-ops state) 20)
+  (g/sample (gen-ops id:post) 20)
 
   ;;; Runners
-  (run-model state (last (g/sample (gen-ops state) 20))))
+  ;(run-model state (last (g/sample (gen-ops state) 20)))
+  )
