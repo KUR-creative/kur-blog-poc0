c3efca0e50a1040eaf22a8959dc6e925b67e0ebf
kur <rhdnfka94@gmail.com>
2022-09-05 14:07:04 +0900

    Generate post html included css and js

--- src/kur/blog/page/post.clj
+++ src/kur/blog/page/post.clj
@@ -1,14 +1,18 @@
 (ns kur.blog.page.post
   (:require
-   [hiccup.core :refer [html]]
+   [hiccup.page :refer [html5 include-css include-js]]
    [kur.blog.md2x :refer [obsidian-html]]
-   [kur.blog.page.template :refer [basic-head]]))
+   [kur.blog.page.template :refer [scale1-viewport charset-utf8]]))
 
-(defn post-html [text]
-  (html (vec (cons :head basic-head))
-        [:body (obsidian-html text)]))
+(defn post-html [text & {:keys [js-paths css-paths]}]
+  (html5 [:head scale1-viewport charset-utf8
+          (apply include-css css-paths)
+          (apply include-js js-paths)]
+         [:body (obsidian-html text)]))
 
 ;;
 (comment
   (def text "# 1 \n ## 2 \n ppap \n\n bbab \n - 1 \n - 22")
-  (spit "out/t.html" (post-html text)))
+  (spit "out/t.html" (post-html text {:css-paths []}))
+  (spit "out/t.html"
+        (post-html text {:css-paths ["test/fixture/css/test-p/red-p.css"]})))
